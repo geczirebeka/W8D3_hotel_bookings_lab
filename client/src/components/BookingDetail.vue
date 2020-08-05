ee0cadd64334223c9ee6dc599389b13cc5d6e930
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-08-05 14:18:07 +0100

    shows full booking information on app

--- client/src/components/BookingDetail.vue
+++ client/src/components/BookingDetail.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
+  <div class="booking">
+      <h2>{{booking.name}}</h2>
+      <p>{{booking.email}}</p>
+      <p>{{booking.checkIn}}</p>
       
+      <button type="button" class="delete-btn" v-on:click="deleteBooking">Delete Booking</button>
   </div>
 </template>
 
 <script>
+import BookingService from '@/services/BookingService.js'
+
 export default {
+    name: 'booking-detail',
+    props: ['booking']
+
 
 }
 </script>
