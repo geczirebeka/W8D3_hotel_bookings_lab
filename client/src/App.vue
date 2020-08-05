9ffe06dcea1413bb8cd967ae859c4fb574686966
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-08-05 14:09:16 +0100

    app data shows objects in database

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <bookings-grid :bookings="bookings" />
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {eventBus} from '@/main.js';
+// import BookingsFrom from './components/BookingsForm.vue';
+import BookingsGrid from './components/BookingsGrid.vue';
+import BookingService from './services/BookingService.js';
 
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      bookings: []
+    };
+  },
   components: {
-    HelloWorld
+    // 'bookings-form': BookingsForm,
+    'bookings-grid': BookingsGrid
+  },
+  mounted() {
+    this.fetchBookings();
+
+    // eventBus.$on('booking-added', (booking) => {
+    //   this.bookings.push(booking);
+    // });
+
+    // eventBus.$on('booking-deleted', (id) => {
+    //   let index = this.bookings.findIndex(booking => booking._id === id);
+    //   this.sightings.splice(index, 1);
+    // })
+  },
+  methods: {
+    fetchBookings() {
+      BookingService.getBookings()
+        .then(bookings => this.bookings = bookings);
+    }
   }
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ client/src/components/BookingsGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="bookingsGrid">
+      <booking-detail v-for="(booking, index) in bookings" :key="index" :booking="booking" />
+  </div>
+</template>
+
+<script>
+import BookingDetail from './BookingDetail.vue'
+
+export default {
+    name: 'bookings-grid',
+    components: {
+        'booking-detail': BookingDetail
+    },
+    props: ['bookings']
+}
+</script>
+
+<style>
+
+</style>
